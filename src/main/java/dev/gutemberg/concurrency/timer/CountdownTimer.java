87b1b95c0515a05291e28a890ae8587e0f97de24
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-09-13 17:55:48 -0300

    refactor code to improve redability

--- src/main/java/dev/gutemberg/concurrency/timer/CountdownTimer.java
+++ src/main/java/dev/gutemberg/concurrency/timer/CountdownTimer.java
@@ -7,25 +7,32 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CountdownTimer {
-    public static void main(String[] args) {
-        final var scanner = new Scanner(System.in);
-        System.out.println("Insert a value to start (00:00:00):");
-        final var input = scanner.next().split(":");
-        int hours = Integer.parseInt(input[0]);
-        int minutes = Integer.parseInt(input[1]);
-        int seconds = Integer.parseInt(input[2]);
-        AtomicInteger totalTimeInSeconds = new AtomicInteger(hours * 60 * 60 + minutes * 60 + seconds);
+    public static void main(final String[] args) {
+        final var totalTimeInSeconds = new AtomicInteger(getTimerValueInSeconds());
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
         scheduledExecutorService.scheduleAtFixedRate(() -> {
             if (totalTimeInSeconds.get() == 0) {
                 System.out.println("Time done!");
                 return;
             }
-            totalTimeInSeconds.getAndDecrement();
-            int hours2 = totalTimeInSeconds.get() / 3600;
-            int minutes2 = (totalTimeInSeconds.get() % 3600) / 60;
-            int seconds2 = totalTimeInSeconds.get() % 60;
-            System.out.printf("%02d:%02d:%02d\n", hours2, minutes2, seconds2);
+            displayTimer(totalTimeInSeconds.decrementAndGet());
         }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public static int getTimerValueInSeconds() {
+        final var scanner = new Scanner(System.in);
+        System.out.println("Insert a value to start (00:00:00):");
+        final var input = scanner.next().split(":");
+        int hours = Integer.parseInt(input[0]);
+        int minutes = Integer.parseInt(input[1]);
+        int seconds = Integer.parseInt(input[2]);
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public static void displayTimer(int totalTimeInSeconds) {
+        int hours = totalTimeInSeconds / 3600;
+        int minutes = (totalTimeInSeconds % 3600) / 60;
+        int seconds = totalTimeInSeconds % 60;
+        System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
     }
 }
